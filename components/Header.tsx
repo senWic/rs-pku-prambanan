040c7b83fafce4f381de3b566c4f1a1c5a84317e
senWic <senoadidr@gmail.com>
2026-08-08 21:25:30 +0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ const PROFIL_LINKS = [
 
 export default function Header() {
   const [open, setOpen] = useState(false);
-
+  
   return (
     <div className="sticky top-0 z-50">
       {/* Top utility bar */}
